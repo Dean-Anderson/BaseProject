520b39a2d53dac7ecb52fa2cb9cf2d4349851a31
dean <dean.anderson@kakaomobility.com>
2022-07-09 21:55:31 +0900

    프로젝트 생성 및 APIClient 구현

---
+++ BaseProject/Shared/BaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseProjectApp.swift
+//  Shared
+//
+//  Created by dean.anderson on 2022/07/09.
+//
+
+import SwiftUI
+
+@main
+struct BaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
